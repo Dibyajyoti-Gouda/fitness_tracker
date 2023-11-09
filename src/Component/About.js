2d7e6898d2e44bd3c8707c60a243506c22d0d778
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-11-10 00:09:59 +0530

    Update About.js

--- src/Component/About.js
+++ src/Component/About.js
@@ -13,10 +13,10 @@ const About = () => {
                   <div className="col-lg-7">
                       <div className="mb-4">
                           <h5 className="text-primary text-uppercase">About Us</h5>
-                          <h1 className="display-3 text-uppercase mb-0">Welcome to Gymster</h1>
+                          <h1 className="display-3 text-uppercase mb-0">Welcome to OnlyFats</h1>
                       </div>
-                      <h4 className="text-body mb-4">Diam dolor diam ipsum tempor sit. Clita erat ipsum et lorem stet no labore lorem sit clita duo justo magna dolore</h4>
-                      <p className="mb-4">Nonumy erat diam duo labore clita. Sit magna ipsum dolor sed ea duo at ut. Tempor sit lorem sit magna ipsum duo. Sit eos dolor ut sea rebum, diam sea rebum lorem kasd ut ipsum dolor est ipsum. Et stet amet justo amet clita erat, ipsum sed at ipsum eirmod labore lorem.</p>
+                      <h4 className="text-body mb-4">Transform the body, transcend the limits. Embrace the burn, earn the results.</h4>
+                      <p className="mb-4">Fitness is not about being better than someone else; it's about being better than you used to be.</p>
                       <div className="rounded bg-dark p-5">
                           <ul className="nav nav-pills justify-content-between mb-3">
                               <li className="nav-item w-50">
@@ -28,12 +28,12 @@ const About = () => {
                           </ul>
                           <div className="tab-content">
                               <div className="tab-pane fade show active" id="pills-1">
-                                  <p className="text-secondary mb-0">Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet diam et eos sadipscing labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor consetetur takimata eirmod, dolores takimata consetetur invidunt magna dolores aliquyam dolores dolore. Amet erat amet et magna</p>
+                                  <p className="text-secondary mb-0">Welcome to [OnlyFats] – your fitness ally! We're more than an app; we're your partner in progress. With personalized plans and a supportive community, we're here to redefine your limits. Let's break barriers together!</p>
                               </div>
                               <div className="tab-pane fade" id="pills-2">
-                                  <p className="text-secondary mb-0">Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet diam et eos sadipscing labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor consetetur takimata eirmod, dolores takimata consetetur invidunt magna dolores aliquyam dolores dolore. Amet erat amet et magna</p>
+                                  <p className="text-secondary mb-0">Welcome to [OnlyFats] – your fitness ally! We're more than an app; we're your partner in progress. With personalized plans and a supportive community, we're here to redefine your limits. Let's break barriers together!</p>
                               </div>
-                          </div>
+                          </div>l
                       </div>
                   </div>
               </div>
