5c9c35dfea994e8eeb11116a68f3e3185c763e86
Krishna Bajpai <114793439+KrIsHnABaJ@users.noreply.github.com>
2023-11-10 00:36:59 +0530

    Update ProgramStart.js

--- src/Component/ProgramStart.js
+++ src/Component/ProgramStart.js
@@ -10,7 +10,7 @@ const ProgramStart = () => {
                       <div className="bg-light rounded text-center p-5">
                           <i className="flaticon-six-pack display-1 text-primary" />
                           <h3 className="text-uppercase my-4">Body Building</h3>
-                          <p>Sed amet tempor amet sit kasd sea lorem dolor ipsum elitr dolor amet kasd elitr duo vero amet amet stet</p>
+                          <p> Build muscle, strength, and definition through weight training and proper nutrition.</p>
                           <a className="text-uppercase" href>Read More <i className="bi bi-arrow-right" /></a>
                       </div>
                   </div>
@@ -18,7 +18,7 @@ const ProgramStart = () => {
                       <div className="bg-light rounded text-center p-5">
                           <i className="flaticon-barbell display-1 text-primary" />
                           <h3 className="text-uppercase my-4">Weight Lefting</h3>
-                          <p>Sed amet tempor amet sit kasd sea lorem dolor ipsum elitr dolor amet kasd elitr duo vero amet amet stet</p>
+                          <p> Use resistance to build strength and muscle through controlled movements with weights.</p>
                           <a className="text-uppercase" href>Read More <i className="bi bi-arrow-right" /></a>
                       </div>
                   </div>
@@ -26,12 +26,12 @@ const ProgramStart = () => {
                       <div className="bg-light rounded text-center p-5">
                           <i className="flaticon-bodybuilding display-1 text-primary" />
                           <h3 className="text-uppercase my-4">Muscle Building</h3>
-                          <p>Sed amet tempor amet sit kasd sea lorem dolor ipsum elitr dolor amet kasd elitr duo vero amet amet stet</p>
+                          <p> Strengthen and grow muscles through targeted resistance training and nutrition.</p>
                           <a className="text-uppercase" href>Read More <i className="bi bi-arrow-right" /></a>
                       </div>
                   </div>
                   <div className="col-lg-12 col-md-6 text-center">
-                      <h1 className="text-uppercase text-light mb-4">30% Discount For This Summer</h1>
+                      <h1 className="text-uppercase text-light mb-4">30% Discount For This Festival Season</h1>
                       <a href className="btn btn-primary py-3 px-5">Become A Member</a>
                   </div>
               </div>
